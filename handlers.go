a06f4814c6cedb44a848bac3ac6ee16bd14ada56
YaroslavalsoraY <jaroslavpodoprigora@yandex.ru>
2025-04-19 06:46:57 +0300

    add base logic

---
+++ handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HandlerHealtzh(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) HandlerMetrics(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	count := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
+	w.Write([]byte(count))
+}
+
+func (cfg *apiConfig) HandlerReset(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	cfg.reset()
+	w.WriteHeader(http.StatusOK)
+}
--- main.go
+++ main.go
@@ -3,12 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	mux := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-	server := http.Server{
+func main() {
+	conf := apiConfig {
+		fileserverHits: atomic.Int32{},
+	}
+	
+	baseHandler := http.FileServer(http.Dir("."))
+
+	mux := http.NewServeMux()
+	mux.Handle("/", conf.middlewareMetricsInc(baseHandler))
+	mux.Handle("/app/", conf.middlewareMetricsInc(http.StripPrefix("/app", baseHandler)))
+	
+	mux.HandleFunc("/api/healthz", HandlerHealtzh)
+	mux.HandleFunc("/admin/metrics", conf.HandlerMetrics)
+	mux.HandleFunc("/admin/reset", conf.HandlerReset)
+
+	server := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
 	}
@@ -17,6 +34,16 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		return
-	}
-	
+	}	
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) reset() {
+	cfg.fileserverHits.Swap(0)
 }
